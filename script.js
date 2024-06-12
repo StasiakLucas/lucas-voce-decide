cdef16ea9941a279171fb78b48f209c520dc510c
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-06-12 23:48:38 +0000

    12/06

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Qual é a cor dos olhos do Dayvison?",
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e aúdios hiper-realistas. Qual o seu primeiro pensamento?",
     alternativas: [
-      "Azuis",
-      "Verdes"
+      "Isso é assustador!",
+      "Isso é maravilhoso!"
     ]
   },
 
   {
-    enunciado: "Quem é o melhor vilão Jason ou Fred Krügger",
+    enunciado: "Com a descoberta desta tecnologia, chamada inteligência Artificial (IA), uma professora de tecnologia de uma escola decidiu fazer uma sequência de aulas sobre essa ferramenta. No fim de uma aula ela pede que você escreva um trabalho sobre o usa da IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Jason",
-      "Fred"
+      "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendimento.",
+      "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
     ]
   },
 
@@ -57,12 +57,11 @@ function mostraAlternativas() {
   }
 }
 
-mostraAlternativas();
 
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
-
+  mostraAlternativas();
 }
 
 mostraPergunta();
